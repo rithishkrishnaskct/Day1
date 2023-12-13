658ddff23170be3879d8215868d24908b99248d7
rithishkrishnaskct <118254753+rithishkrishnaskct@users.noreply.github.com>
2023-12-13 14:43:16 +0530

    Add files via upload

---
+++ Day_1/App.js
@@ -0,0 +1,15 @@
+import LoginForm from './Components/LoginForm/LoginForm';
+import Register from './Components/Register/Register';
+
+function App() {
+  return (
+    <div>
+     <LoginForm />
+       {/*<Register/>*/}
+      
+    
+    </div>
+  );
+}
+
+export default App;
